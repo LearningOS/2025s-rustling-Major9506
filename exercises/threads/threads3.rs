ec3110868ad08428d3abb4a882c23c7622975ec0
xiaojunxiang <1293963839@qq.com>
2025-04-16 07:06:41 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,12 +1,8 @@
 // threads3.rs
-//
-// Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -26,26 +22,30 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
+fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> Vec<thread::JoinHandle<()>> {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
-    thread::spawn(move || {
+    let tx1 = tx.clone();
+    let handle1 = thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::spawn(move || {
+    let tx2 = tx.clone();
+    let handle2 = thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    vec![handle1, handle2]
 }
 
 fn main() {
@@ -53,14 +53,31 @@ fn main() {
     let queue = Queue::new();
     let queue_length = queue.length;
 
-    send_tx(queue, tx);
+    let handles = send_tx(queue, tx);
 
-    let mut total_received: u32 = 0;
-    for received in rx {
-        println!("Got: {}", received);
-        total_received += 1;
+    // 使用 Arc 和 Mutex 来共享和修改 total_received
+    let total_received = Arc::new(Mutex::new(0));
+    let total_received_clone = Arc::clone(&total_received);
+
+    // 使用一个线程来接收消息
+    let handle_rx = thread::spawn(move || {
+        for received in rx {
+            println!("Got: {}", received);
+            let mut count = total_received_clone.lock().unwrap();
+            *count += 1;
+        }
+    });
+
+    // 等待所有发送线程完成
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    // 等待接收线程完成
+    handle_rx.join().unwrap();
+
+    // 获取 total_received 的值
+    let final_count = total_received.lock().unwrap();
+    println!("total numbers received: {}", *final_count);
+    assert_eq!(*final_count, queue_length);
 }
